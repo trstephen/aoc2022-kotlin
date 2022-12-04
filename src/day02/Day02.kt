0d4af02ab5b24a748eb094f050878e7a0b9bca87
Tyler Stephen <tyler.stephen@gmail.com>
2022-12-04 14:23:41 -0800

    Day02 Part02

--- src/day02/Day02.kt
+++ src/day02/Day02.kt
@@ -5,7 +5,7 @@ import java.io.File
 object Day02 {
 
     @JvmStatic
-    fun main(args: Array<String>) = part1()
+    fun main(args: Array<String>) = part2()
 
     fun part1() {
         val strat = readStrategy("src/day02/part01-input.txt")
@@ -15,17 +15,26 @@ object Day02 {
     fun readStrategy(filename: String): List<RPSStrategy> =
         File(filename).readLines().map {
             val parts = it.split(" ")
-            RPSStrategy(me = RPS.fromMyChoice(parts[1]), them = RPS.fromOpponentChoice(parts[0]))
+            RPSStrategy(them = RPS.fromOpponentChoice(parts[0]), me = RPS.fromMyChoice(parts[1]))
+        }
+
+    fun part2() {
+        val strat = readStrategy2("src/day02/part01-input.txt")
+        println(strat.sumOf { it.score() }) // 12725
+    }
+
+    fun readStrategy2(filename: String): List<RPSStrategy2> =
+        File(filename).readLines().map {
+            val parts = it.split(" ")
+            RPSStrategy2(them = RPS.fromOpponentChoice(parts[0]), result = RPSResult.fromString(parts[1]))
         }
 }
 
-data class RPSStrategy(val me: RPS, val them: RPS) {
+data class RPSStrategy(val them: RPS, val me: RPS) {
     fun score(): Int {
         val result = when {
-            me == them -> RPSResult.DRAW
-            (me == RPS.ROCK && them == RPS.SCISSORS) ||
-                    (me == RPS.SCISSORS && them == RPS.PAPER) ||
-                    (me == RPS.PAPER && them == RPS.ROCK) -> RPSResult.WIN
+            them == me -> RPSResult.DRAW
+            them.losesTo() == me -> RPSResult.WIN
             else -> RPSResult.LOSE
         }
 
@@ -33,15 +42,50 @@ data class RPSStrategy(val me: RPS, val them: RPS) {
     }
 }
 
+data class RPSStrategy2(val them: RPS, val result: RPSResult) {
+
+    fun score(): Int {
+        val myRPS = when (result) {
+            RPSResult.WIN -> them.losesTo()
+            RPSResult.DRAW -> them
+            RPSResult.LOSE -> them.beats()
+        }
+
+        return myRPS.selectionScore + result.score
+    }
+}
+
 enum class RPSResult(val score: Int) {
-    WIN(6), DRAW(3), LOSE(0)
+    WIN(6), DRAW(3), LOSE(0);
+
+    companion object {
+        fun fromString(s: String) = when (s) {
+            "X" -> LOSE
+            "Y" -> DRAW
+            "Z" -> WIN
+            else -> error("Unknown RPSResult selection '$s")
+        }
+    }
 }
 
 enum class RPS(val selectionScore: Int) {
 
     // TIL: Can't model 'beats: RPS' here because 'SCISSORS' isn't init'd when you
     // define 'ROCK'. No problem for the others since the value is the n-1 entry.
+    // Work around this with 'beats()' and 'losesTo()'.
     ROCK(1), PAPER(2), SCISSORS(3);
+
+    fun beats() = when (this) {
+        ROCK -> SCISSORS
+        PAPER -> ROCK
+        SCISSORS -> PAPER
+    }
+
+    fun losesTo() = when (this) {
+        ROCK -> PAPER
+        PAPER -> SCISSORS
+        SCISSORS -> ROCK
+    }
 
     companion object {
         fun fromOpponentChoice(c: String) = when (c) {

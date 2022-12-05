d526713c8fae9f035b705dc3a4c158f75f0e3840
Tyler Stephen <tyler.stephen@gmail.com>
2022-12-04 18:40:07 -0800

    Day04 Part02

--- src/day04/Day04.kt
+++ src/day04/Day04.kt
@@ -5,11 +5,16 @@ import java.io.File
 object Day04 {
 
     @JvmStatic
-    fun main(args: Array<String>) = part1()
+    fun main(args: Array<String>) = part2()
 
     fun part1() {
         val assignments = readAssignments("src/day04/part01-input.txt")
         println(assignments.count { it.isInclusive() }) // 462
+    }
+
+    fun part2() {
+        val assignments = readAssignments("src/day04/part01-input.txt")
+        println(assignments.count { it.overlaps() }) // 835
     }
 
     fun readAssignments(filename: String): List<Pair<Assignment, Assignment>> =
@@ -20,19 +25,30 @@ object Day04 {
             }
             Pair(a1, a2)
         }
-}
 
-data class Assignment(val startSection: Int, val endSection: Int) {
+    data class Assignment(val startSection: Int, val endSection: Int) {
+
+        /**
+         * Do the inclusive bounds of [this] contain [other]?
+         */
+        fun contains(other: Assignment): Boolean =
+            this.startSection <= other.startSection && this.endSection >= other.endSection
+    }
 
     /**
-     * Do the inclusive bounds of [this] contain [other]?
+     * One of [first] or [second] contains the other.
      */
-    fun contains(other: Assignment): Boolean =
-        this.startSection <= other.startSection && this.endSection >= other.endSection
-}
+    fun Pair<Assignment, Assignment>.isInclusive(): Boolean =
+        first.contains(second) || second.contains(first)
 
-/**
- * One of [first] or [second] contains the other.
- */
-fun Pair<Assignment, Assignment>.isInclusive(): Boolean =
-    first.contains(second) || second.contains(first)
+    /**
+     * Is an edge of value [this] inside [a]?
+     */
+    fun Int.inside(a: Assignment): Boolean = this in a.startSection .. a.endSection
+
+    /**
+     * Is there any overlap between the inclusive bounds of [first] and [second]?
+     */
+    fun Pair<Assignment, Assignment>.overlaps(): Boolean =
+        first.startSection.inside(second) || first.endSection.inside(second) || first.contains(second)
+}

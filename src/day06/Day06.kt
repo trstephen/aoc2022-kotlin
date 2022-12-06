b28d046113a79770242aecc8ba236cd4f31d5a2a
Tyler Stephen <tyler.stephen@gmail.com>
2022-12-05 22:41:05 -0800

    Day06 Part02

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -5,17 +5,21 @@ import java.io.File
 object Day06 {
 
     @JvmStatic
-    fun main(args: Array<String>) = part1()
+    fun main(args: Array<String>) = part2()
 
     val input = File("src/day06/input.txt").readLines().first()
 
     fun part1() {
-        // val example = examples1.map { (s, expected) -> s.findPart1() to expected }
-        println(input.findPart1())
+        // val example = examples1.map { (s, expected) -> s.findUniqueWindow(4) to expected }
+        println(input.findUniqueWindow(windowSize = 4)) // 1175
     }
 
-    fun String.findPart1(): Int {
-        val windowSize = 4
+    fun part2() {
+        // val example = examples2.map { (s, expected) -> s.findUniqueWindow(14) to expected }
+        println(input.findUniqueWindow(windowSize = 14)) // 3217
+    }
+
+    fun String.findUniqueWindow(windowSize: Int): Int {
         val windowIdx = this.asSequence()
             .windowed(size = windowSize)
             .indexOfFirst { it.toSet().size == windowSize } // no dupes in window!
@@ -32,4 +36,12 @@ val examples1 = listOf(
     "nppdvjthqldpwncqszvftbrmjlhg" to 6,
     "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" to 10,
     "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" to 11,
+)
+
+val examples2 = listOf(
+    "mjqjpqmgbljsphdztnvjfqwrcgsmlb" to 19,
+    "bvwbjplbgvbhsrlpgdmjqwftvncz" to 23,
+    "nppdvjthqldpwncqszvftbrmjlhg" to 23,
+    "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg" to 29,
+    "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw" to 26,
 )

7823cc1299ad08fd861deac7f958b056eec20b87
Tyler Stephen <tyler.stephen@gmail.com>
2022-12-10 17:39:05 -0800

    Day08 Part02

--- src/day08/Day08.kt
+++ src/day08/Day08.kt
@@ -5,7 +5,7 @@ import java.io.File
 object Day08 {
 
     @JvmStatic
-    fun main(args: Array<String>) = part1()
+    fun main(args: Array<String>) = part2()
 
     val input = File("src/day08/input.txt").readLines()
 
@@ -20,6 +20,20 @@ object Day08 {
 
         val total = reachable.sumOf { it.count { it } }
         println(total) // 1812
+    }
+
+    fun part2() {
+        val forest = input.toForest()
+        // forest.treeHeights.prettyPrint()
+
+        // println()
+
+        val scenicScores = forest.treeHeights.mapIndexed { rowIdx, row ->
+            row.mapIndexed { colIdx, _ -> forest.scenicScoreFor(rowIdx, colIdx) }
+        }
+        // scenicScores.prettyPrint()
+
+        println(scenicScores.flatten().max()) // 315495
     }
 
     data class Forest(val treeHeights: List<List<Int>>, val numRows: Int, val numCols: Int) {
@@ -48,6 +62,41 @@ object Day08 {
         private fun Int.isTallerThan(l: List<Int>): Boolean =
             if (l.isEmpty()) true
             else l.max() < this
+
+        fun scenicScoreFor(rowIdx: Int, colIdx: Int): Int {
+            val h = treeHeights[rowIdx][colIdx]
+
+            val viewLeft = row(rowIdx).take(colIdx).reversed()
+            val scenicLeft = viewLeft.scenicScoreAt(h)
+
+            val viewRight = row(rowIdx).takeLast(numCols - colIdx - 1)
+            val scenicRight = viewRight.scenicScoreAt(h)
+
+            val viewUp = col(colIdx).take(rowIdx).reversed()
+            val scenicUp = viewUp.scenicScoreAt(h)
+
+            val viewDown = col(colIdx).takeLast(numRows - rowIdx - 1)
+            val scenicDown = viewDown.scenicScoreAt(h)
+
+            return listOf(
+                scenicLeft, scenicRight, scenicUp, scenicDown
+            ).map { it.coerceAtLeast(1) }.reduce { a, b -> a * b }
+        }
+
+        private fun List<Int>.scenicScoreAt(height: Int): Int {
+            val treesLessThanHeight = this.takeWhile { it < height }.size
+
+            // Did we stop because we ran out of trees or hit one of the same height?
+            // lmao my fanciness failed.
+            return if (treesLessThanHeight == this.size) treesLessThanHeight else treesLessThanHeight + 1
+        }
+
+        private fun List<Int>.takeScenicAt(height: Int): Set<Int> {
+            val scenic = takeWhile { it <= height }.toSet() // 'toSet' deals with repeats where it == height
+
+            // so we don't obliterate the product in 'reduce'
+            return scenic.ifEmpty { setOf(1) }
+        }
     }
 
     fun List<String>.toForest(): Forest {

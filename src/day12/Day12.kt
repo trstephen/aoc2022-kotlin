ead47b6af9f1178a6351733ccce6291bd042d98b
Tyler Stephen <tyler.stephen@gmail.com>
2022-12-21 14:03:39 -0800

    Day 12 skeleton ☠️
    So I can test multi-machine GH setup

---
+++ src/day12/Day12.kt
@@ -0,0 +1,15 @@
+package day12
+
+import java.io.File
+
+object Day12 {
+
+    @JvmStatic
+    fun main(args: Array<String>) = part1()
+
+    val input = File("src/day12/input.txt").readLines()
+
+    fun part1() {
+        TODO()
+    }
+}
